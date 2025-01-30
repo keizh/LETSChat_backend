469b4e210f6e0a09d8e519ccf6cc8880e4f89faf
Keizh <krishnamulay05@gmail.com>
2025-01-30 12:47:03 +0530

    first commit

---
+++ src/api/index.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import express from "express";
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (socket) => {
+  socket.send("WebSocket connection has been established");
+
+  // ⚠️ CLIENT SHOULD ALWAYS SEND STRINGIFIED DATA
+  // ⚠️ SERVER ALWAYS RECIEVES BUFFER
+  // convert BUFFER to STRING using toString()
+  // convert STRING to JSON.parse provided you were expecting a JS OBJECT
+  socket.on("message", (obj) => {});
+});
+
+server.listen(process.env.PORT, () =>
+  console.log(`HTTP & WS server is ONLINE`)
+);
